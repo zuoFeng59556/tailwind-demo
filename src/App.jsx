436c1cfbb6d8306dae34957e9412e714aee1a050
左风 <595563214@qq.com>
2023-02-12 02:07:53 +0800

    feat: 提交完整代码

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="mx-4 my-4">
+        <img className="rounded  mx-auto w-2/5 shadow-lg"  src="/jay.png" alt="" />
+        <div className="my-8 border rounded md:flex md:flex-wrap shadow-lg overflow-hidden">
+          
+          <div className="p-6 border-b md:w-1/2 md:border-r lg:w-1/4 lg:border-r-0 hover:bg-gray-800 group">
+            <h2 className="text-base md:text-lg lg:text-xl font-bold mb-2 group-hover:text-white">爱在西元前</h2>
+            <p className="text-sm md:text-base lg:text-lg text-gray-500 group-hover:text-white">古巴比伦王颁布了汉谟拉比法典刻在黑色的玄武岩距今已经三千七百多年你在橱窗前 凝视碑文的字眼我却在旁静静欣赏你那张我深爱的脸祭司 神殿 征战 弓箭 是谁的从前喜欢在人潮中你只属于我的那画面经过苏美女神身边 我以女神之名许愿思念像底格里斯河般的蔓延当古文明只剩下难解的语言传说就成了永垂不朽的诗篇
+            </p>
+          </div>
+
+          <div className="p-6 border-b md:w-1/2 lg:w-1/4 lg:border-r lg:border-r-0 hover:bg-gray-800 group">
+            <h2 className="text-base md:text-lg lg:text-xl font-bold mb-2 group-hover:text-white">爱在西元前</h2>
+            <p className="text-sm md:text-base lg:text-lg text-gray-500 group-hover:text-white">古巴比伦王颁布了汉谟拉比法典刻在黑色的玄武岩距今已经三千七百多年你在橱窗前 凝视碑文的字眼我却在旁静静欣赏你那张我深爱的脸祭司 神殿 征战 弓箭 是谁的从前喜欢在人潮中你只属于我的那画面经过苏美女神身边 我以女神之名许愿思念像底格里斯河般的蔓延当古文明只剩下难解的语言传说就成了永垂不朽的诗篇
+            </p>
+          </div>
+
+          <div className="p-6 border-b md:w-1/2 md:border-r lg:w-1/4 hover:bg-gray-800 group">
+            <h2 className="text-base md:text-lg lg:text-xl font-bold mb-2 group-hover:text-white">爱在西元前</h2>
+            <p className="text-sm md:text-base lg:text-lg text-gray-500 group-hover:text-white">古巴比伦王颁布了汉谟拉比法典刻在黑色的玄武岩距今已经三千七百多年你在橱窗前 凝视碑文的字眼我却在旁静静欣赏你那张我深爱的脸祭司 神殿 征战 弓箭 是谁的从前喜欢在人潮中你只属于我的那画面经过苏美女神身边 我以女神之名许愿思念像底格里斯河般的蔓延当古文明只剩下难解的语言传说就成了永垂不朽的诗篇
+            </p>
+          </div>
+
+          <div className="p-6 md:w-1/2 lg:w-1/4 hover:bg-gray-800 group">
+            <h2 className="text-base md:text-lg lg:text-xl font-bold mb-2 group-hover:text-white">爱在西元前</h2>
+            <p className="text-sm md:text-base lg:text-lg text-gray-500 group-hover:text-white">古巴比伦王颁布了汉谟拉比法典刻在黑色的玄武岩距今已经三千七百多年你在橱窗前 凝视碑文的字眼我却在旁静静欣赏你那张我深爱的脸祭司 神殿 征战 弓箭 是谁的从前喜欢在人潮中你只属于我的那画面经过苏美女神身边 我以女神之名许愿思念像底格里斯河般的蔓延当古文明只剩下难解的语言传说就成了永垂不朽的诗篇
+            </p>
+          </div>
+
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
